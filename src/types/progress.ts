3b4767c374c270b8e20bb87bd40b3b04c0454f66
xiliourt <dyl.sec.ure@gmail.com>
2025-09-19 20:52:59 +1000

    Update progress.ts

--- src/types/progress.ts
+++ src/types/progress.ts
@@ -1,16 +1,14 @@
-export type ProgressItem = {
-    id: string;
-    type: 'track' | 'album' | 'playlist' | 'artist';
-    title: string;
-    progress: number;
-    message: string;
-    status?: 'queued' | 'downloading' | 'completed' | 'error' | 'skipped';
-    speed?: number;
-    startTime?: number;
-    downloadedBytes?: number;
-    stream?: ArrayBuffer;
-    fileExtension?: string;
-    items?: { [id: string]: ProgressItem };
-}
-
-export type SetProgressFn = (update: ProgressItem) => void;
+export type ProgressItemFn = (
+    id: string,
+    type: 'track' | 'album' | 'playlist' | 'artist',
+    title: string,
+    progress: number,
+    message: string,
+    status?: 'queued' | 'downloading' | 'completed' | 'error' | 'skipped',
+    speed?: number,
+    startTime?: number,
+    downloadedBytes?: number,
+    stream?: ArrayBuffer,
+    fileExtension?: string,
+    items?: { [id: string]: ProgressItem }
+) => void,
