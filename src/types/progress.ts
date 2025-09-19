5263c8b3fa06c9cc4392058e964f277711b6edf5
xiliourt <dyl.sec.ure@gmail.com>
2025-09-19 20:58:26 +1000

    Update progress.ts

--- src/types/progress.ts
+++ src/types/progress.ts
@@ -1,3 +1,18 @@
+export type ProgressItem = {
+    id: string;
+    type: 'track' | 'album' | 'playlist' | 'artist';
+    title: string;
+    progress: number;
+    message: string;
+    status?: 'queued' | 'downloading' | 'completed' | 'error' | 'skipped';
+    speed?: number;
+    startTime?: number;
+    downloadedBytes?: number;
+    stream?: ArrayBuffer;
+    fileExtension?: string;
+    items?: { [tidalResourceId: string]: ProgressItem };
+}
+
 export type ProgressItemFn = (
     id: string,
     type: 'track' | 'album' | 'playlist' | 'artist',
