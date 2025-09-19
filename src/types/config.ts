35007f6a5e6806a9e044789c427d5f4d71f88c64
xiliourt <dyl.sec.ure@gmail.com>
2025-09-19 20:59:55 +1000

    Update config.ts

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,14 +1,14 @@
-export type Config (
+export type Config = {
     template: {
-        track: string,
-        video: string,
-        album: string,
-        playlist: string,
+        track: string;
+        video: string;
+        album: string;
+        playlist: string;
     };
     download: {
-        quality: 'LOW' | 'HIGH' | 'LOSSLESS' | 'HI_RES_LOSSLESS',
-        singles_filter: 'none' | 'only' | 'include',
-        download_video: boolean,
-        threads: number,
-    }
-)
+        quality: 'LOW' | 'HIGH' | 'LOSSLESS' | 'HI_RES_LOSSLESS';
+        singles_filter: 'none' | 'only' | 'include';
+        download_video: boolean;
+        threads: number;
+    };
+};
