f33617619017342f5d31fbbd30e88cc0e17ae44b
xiliourt <dyl.sec.ure@gmail.com>
2025-09-19 20:51:28 +1000

    Update download.ts

--- src/types/download.ts
+++ src/types/download.ts
@@ -1,15 +1,25 @@
 import type { TidalApiItem } from '@/types/tidal';
 import type { AuthResponse } from '@/types/auth';
 import type { Config } from '@/types/config';
-import type { ProgressItem } from '@/types/progress';
+import type { ProgressItem, SetProgressFn } from '@/types/progress';
 
-export type DownloadItemParams = {
-    item: TidalApiItem;
-    auth: AuthResponse;
-    config: Config;
-    setProgress: SetProgressFn;
-    dirHandle: FileSystemDirectoryHandle;
-}
+export type DownloadItemFn = (
+    item: TidalApiItem,
+    auth: AuthResponse,
+    config: Config,
+    setProgress: SetProgressFn,
+    dirHandle: FileSystemDirectoryHandle,
+) => Promise<void>;
 
-export type DownloadItemFn = (params: DownloadItemParams) => Promise<void>;
+
+export type DownloadSpecificItemFn = (
+    itemId: string,
+    auth: AuthResponse,
+    config: Config,
+    setProgress: SetProgressFn,
+    dirHandle: FileSystemDirectoryHandle
+) => Promise<void>;
+
+
+
 
