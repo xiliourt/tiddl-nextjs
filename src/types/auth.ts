aa1d9bca3dcb8dfc502402e4fd13273778c28f19
xiliourt <dyl.sec.ure@gmail.com>
2025-09-19 21:07:45 +1000

    Update auth.ts

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -29,3 +29,9 @@ export type AuthResponse = {
     };
     user_id: number;
 }
+
+// Not utilised; simply for reference - the functions are typed in file
+export type getDeviceAuth = () => Promise<AuthDeviceResponse>;
+export type getToken = (deviceCode: string) => Promise<AuthResponse>;
+export type refreshToken = (refreshToken: string) => Promise<AuthResponse>
+
